163118a779ce3ad986b01945e7f49ff4076c82f2
davidzie <davidzi@post.bgu.ac.il>
2021-04-12 20:17:35 +0300

    Add method to parse port string into correct enum port type by using board enum.

--- src/main/java/Enums/IEv3Port.java
+++ src/main/java/Enums/IEv3Port.java
@@ -1,4 +1,13 @@
 package Enums;
 
 public interface IEv3Port extends IPortEnums {
+
+    static IEv3Port getPortType(String port) {
+        char portChar = port.charAt(0);
+        if (portChar >= 65 && portChar <= 68) {
+            return Ev3DrivePort.valueOf(port);
+        } else {
+            return Ev3SensorPort.valueOf(port);
+        }
+    }
 }
